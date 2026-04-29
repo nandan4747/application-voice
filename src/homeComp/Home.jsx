722b76880622f4e67767e1c3e899a5467ee3aa2a
nandan4747 <nandankumar474747@gmail.com>
2026-04-29 20:03:56 +0530

    added some stuffs

--- src/homeComp/Home.jsx
+++ src/homeComp/Home.jsx
@@ -24,7 +24,8 @@ const Home = () => {
             seeMore={true}
             apiUrl={"user/songs/recent"}
             title={"New Releases"}
-          />}
+          />
+        }
 
         {genre && (
           <div className={Styles.genre_holder}>
@@ -39,25 +40,55 @@ const Home = () => {
           </div>
         )}
         {
-        
+          <SongGrid
+            cursorKey="mostPlayed"
+            seeMore={true}
+            apiUrl={"user/mostplayed"}
+            title={"Most played"}
+          />
+        }
 
-        <SongGrid
-          cursorKey="mostPlayed"
-          seeMore={true}
-          apiUrl={"user/mostplayed"}
-          title={"Most played"}
-        /> }
+        {
+          //most played songs by genre
+          <SongListSection
+            apiUrl={`user/songs/recent/genre?genre=${genre}`}
+            title={`Most played Songs according to your teast`}
+          />
+        }
         {
           // most liked songs
-          
+
           <SongGrid apiUrl={"user/mostliked"} title={"Most Liked"} />
-          
+        }
+
+        {
+          //forgotten songs
+          <SongListSection
+            apiUrl={`user/songs/forgottenhits`}
+            title={`Forgotten Hits`}
+          />
         }
 
         <footer className={Styles.footer}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#ff4747"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-circle-alert-icon lucide-circle-alert"
+          >
+            <circle cx="12" cy="12" r="10" />
+            <line x1="12" x2="12" y1="8" y2="12" />
+            <line x1="12" x2="12.01" y1="16" y2="16" />
+          </svg>
           <p className={Styles.disclaimer}>
-            🎵 This is a fun personal project for educational purposes. We do
-            not promote or support music piracy. Please support artists by using
+            This is a fun personal project for educational purposes. We do not
+            promote or support music piracy. Please support artists by using
             official streaming services.
           </p>
         </footer>
